c8ee91b82fef646c44f8372667f8d156b4592b1d
gabymenji <gabymenji@gmail.com>
2024-03-03 03:10:34 +0000

    Classes com os atributos

---
+++ OperationControl/Pages/Verificação.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OperationControl.Pages
+{
+    public class VerificaçãoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
